e14a226bc897b0b203916a90fb88fe4be956f658
dammytrager <damilaremalomo@gmail.com>
2019-02-10 13:32:49 +0100

    Connection.php Updated

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$host = 'us-cdbr-iron-east-03.cleardb.net';
+$user = 'bce1f3a10927be';
+$password = '19e7c7d1';
+$database = 'heroku_2d7302eda77b267';
+
+$db = mysqli_connect($host, $user, $password, $database);
