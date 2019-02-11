1d9cf0d1c95a5afff7946e1da001c455a2730a5d
dammytrager <damilaremalomo@gmail.com>
2019-02-11 15:40:44 +0100

    Headers set

--- connection.php
+++ connection.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header("Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description");
+
 $host = 'us-cdbr-iron-east-03.cleardb.net';
 $user = 'bce1f3a10927be';
 $password = '19e7c7d1';
--- mailer.php
+++ mailer.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header("Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description");
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
